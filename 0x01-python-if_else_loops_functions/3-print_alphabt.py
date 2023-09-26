cf1f9848ba127d68801db7b9ec697a256e9f3987
MaryomaElsayed <maryamelsayd.1@gmail.com>
2023-09-26 09:43:51 +0000

    ggtt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for x in range(ord("a"),ord("z") +1):
-  if x != ord("e") and x != ord("q"):
-    print("{}".format(chr(x)) ,end = "")
+
+for x in range(ord("a"), ord("z") + 1):
+    if x != ord("e") and x != ord("q"):
+        print("{}".format(chr(x)), end="")
