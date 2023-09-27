7c0bf123f2ce17ae45c951e736c3d6c0982e43d5
MaryomaElsayed <maryamelsayd.1@gmail.com>
2023-09-27 01:49:16 +0000

    DONE

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,15 +1,4 @@
-Chat with AI Online
-Simply ask your AI chatbot assistant to generate!
-
-assistant avatar
-Hello! How can I help you today?
-#!/usr/bin/python3 """Define classes for a singly-linked list."""   class Node:     """Represent a node in a singly-linked list."""      def __init__(self, data, next_node=None):         """Initialize a new Node.          Args:             data (int): The data of the new Node.             next_node (Node): The next node of the new Node.         """         self.data = data         self.next_node = next_node      @property     def data(self):         """Get/set the data of the Node."""         return (self.__data)      @data.setter     def data(self, value):         if not isinstance(value, int):             raise TypeError("data must be an integer")         self.__data = value      @property     def next_node(self):         """Get/set the next_node of the Node."""         return (self.__next_node)      @next_node.setter     def next_node(self, value):         if not isinstance(value, Node) and value is not None:             raise TypeError("next_node must be a Node object")         self.__next_node = value   class SinglyLinkedList:     """Represent a singly-linked list."""      def __init__(self):         """Initalize a new SinglyLinkedList."""         self.__head = None      def sorted_insert(self, value):         """Insert a new Node to the SinglyLinkedList.          The node is inserted into the list at the correct         ordered numerical position.          Args:             value (Node): The new Node to insert.         """         new = Node(value)         if self.__head is None:             new.next_node = None             self.__head = new         elif self.__head.data > value:             new.next_node = self.__head             self.__head = new         else:             tmp = self.__head             while (tmp.next_node is not None and                     tmp.next_node.data < value):                 tmp = tmp.next_node             new.next_node = tmp.next_node             tmp.next_node = new      def __str__(self):         """Define the print() representation of a SinglyLinkedList."""         values = []         tmp = self.__head         while tmp is not None:             values.append(str(tmp.data))             tmp = tmp.next_node         return ('\n'.join(values)) write this code in pycodestyle
-assistant avatar
-Certainly! Here's your code refactored to comply with Pycodestyle conventions:
-
-```python
 #!/usr/bin/python3
-
 """Define classes for a singly-linked list."""
 
 
@@ -29,7 +18,7 @@ class Node:
     @property
     def data(self):
         """Get/set the data of the Node."""
-        return self.__data
+        return (self.__data)
 
     @data.setter
     def data(self, value):
@@ -40,7 +29,7 @@ class Node:
     @property
     def next_node(self):
         """Get/set the next_node of the Node."""
-        return self.__next_node
+        return (self.__next_node)
 
     @next_node.setter
     def next_node(self, value):
@@ -74,8 +63,8 @@ class SinglyLinkedList:
             self.__head = new
         else:
             tmp = self.__head
-            while tmp.next_node is not None and \
-                    tmp.next_node.data < value:
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
                 tmp = tmp.next_node
             new.next_node = tmp.next_node
             tmp.next_node = new
@@ -87,4 +76,4 @@ class SinglyLinkedList:
         while tmp is not None:
             values.append(str(tmp.data))
             tmp = tmp.next_node
-        return '\n'.join(values)
+        return ('\n'.join(values))
